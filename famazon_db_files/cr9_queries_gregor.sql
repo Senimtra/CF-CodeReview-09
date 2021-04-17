c68c508eb80e699385193bfba7f54cf2131c81aa
Senimtra <senimtra@hotmail.com>
2021-04-17 09:27:53 +0200

    add queries 3 + 4

--- famazon_db_files/cr9_queries_gregor.sql
+++ famazon_db_files/cr9_queries_gregor.sql
@@ -13,6 +13,7 @@ FROM company
     ON product_id = fk_product_id
 WHERE comp_name = "Chatterbridge";
 
+
 /*############
 ### 2. Report:
 ###    Who purchased products on 19.04.2020?
@@ -28,3 +29,44 @@ FROM customer
     INNER JOIN purchase
     ON cart_id = fk_cart_id
 WHERE order_date = "2020-04-19";
+
+
+/*############
+### 3. Report:
+###    What products were sent between 01.01.2021 and 31.03.2021?
+###    -> 10 different products were shipped */
+
+SELECT CONCAT(prod_name, ": ", amount, " items")
+AS "These products were sent between 01.01.21 and 31.03.21:", ship_date
+AS "shipped:"
+FROM product
+    INNER JOIN website
+    ON product_id = fk_product_id
+    INNER JOIN purchase
+    ON cart_id = fk_cart_id
+    INNER JOIN shipping
+    ON order_id = fk_order_id
+WHERE ship_date > "2020-12-31"
+    AND ship_date < "2021-04-01"
+ORDER BY ship_date;
+
+
+/*############
+### 4. Report:
+###    How many products where sent to Yangba in China?
+###    -> 3 orders with 3 different products */
+
+SELECT CONCAT(COUNT(prod_name), " different products sent to ", city, ", ", country, ".")
+AS "There were .."
+FROM product
+    INNER JOIN website
+    ON product_id = fk_product_id
+    INNER JOIN purchase
+    ON cart_id = fk_cart_id
+    INNER JOIN shipping
+    ON order_id = fk_order_id
+    INNER JOIN carrier
+    ON carrier_id = fk_carrier_id
+    INNER JOIN zip
+    ON zip_code = fk_zip_code
+WHERE city = "Yangba";
